8129b09d2f7b5c5f8830a3c9a024a3ec476ced10
PAAWSA09LC0913 <Duoc@PAAWSA09LC0913.duoc.acad>
2025-11-04 21:56:42 -0300

    avance clases Router

---
+++ src/pages/home.js
@@ -0,0 +1,21 @@
+import react from 'react';
+import NavBarPrincipal from './components/navbar';
+import { Nav } from 'react-bootstrap';
+
+
+function HomePrincipal () {
+    return (
+        <div>
+            <NavBarPrincipal />
+        <div className='row mt-4'>
+            <div className='col-md-12'>
+                <h2> Pagina Principal</h2>
+                <p>jfhsdfasvasñs hasljidhas v lsdh isadvb skdhsiaud a</p> 
+            </div>
+        </div>
+    </div>
+    );
+
+}
+
+export default HomePrincipal;
---
+++ src/pages/product.js
@@ -0,0 +1,27 @@
+import react from 'react';
+import NavBarPrincipal from './components/navbar';
+import { Nav } from 'react-bootstrap';
+
+function ProductPrincipal () {
+    return (
+        <div>
+            <NavBarPrincipal />
+        
+        <div className='row'>
+            <div className='col-md-12'>
+                <h2> Productos Destacados</h2>
+                <p>1516231 1651621 1635164984 b68465164 64651654
+                1516231 1651621 1635164984 b68465164 64651654
+                1516231 1651621 1635164984 b68465164 64651654
+                1516231 1651621 1635164984 b68465164 64651654
+                1516231 1651621 1635164984 b68465164 64651654
+                1516231 1651621 1635164984 b68465164 64651654</p> 
+            </div>
+        </div>
+    </div>
+    );
+
+}
+
+
+export default ProductPrincipal;
